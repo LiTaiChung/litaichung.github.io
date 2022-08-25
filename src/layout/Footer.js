333231ced730d5ce952037ee674a535b4c2aac47
Ice <ice@IcedeMacBook-Pro.local>
2022-08-25 17:01:57 +0800

    update route, header

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { Link } from "react-router-dom";
 
 export default function Footer() {
     return (
@@ -17,7 +18,7 @@ export default function Footer() {
                 </ul>
             </div>
             <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-            <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <Link to="" className="hover:underline">Ice</Link>. All Rights Reserved.
+            <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <Link to="/" className="hover:underline">Ice</Link>. All Rights Reserved.
             </span>
         </footer>
     )
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,15 +2,17 @@ import Layout from 'layout';
 import Page403 from 'pages/403';
 import Page404 from 'pages/404';
 import Home from 'pages/Home';
-import News from 'pages/News/topic'
+import Project from 'pages/Project';
+import ProjectItem from 'pages/Project/ProjectItem';
+import Contact from 'pages/Contact';
 
 const routes = [
     {
-        path: '/403',
+        path: '403',
         element: <Page403 />,
     },
     {
-        path: '/404',
+        path: '404',
         element: <Page404 />,
     },
     {
@@ -21,19 +23,19 @@ const routes = [
                 index: true,
                 element: <Home />,
             },
-
             {
-                path: '/news',
+                path: 'project',
+                element: <Project />,
                 children: [
                     {
-                        path: 'topic',
-                        element: <News />,
-                    },
-                    {
-                        path: ':newId',
-                        element: <Home />,
+                        path: ':projectId',
+                        element: <ProjectItem />,
                     },
                 ]
+            },
+            {
+                path: 'contact',
+                element: <Contact />,
             },
         ],
     }
