4af08c8413725fec621a3135a00fd1b5b27bd239
Ice <ice@IcedeMacBook-Pro.local>
2022-08-26 00:36:12 +0800

    update dark content

--- src/pages/Project/index.js
+++ src/pages/Project/index.js
@@ -24,7 +24,7 @@ export default function Project() {
                                 />
                             </svg>
                             <input
-                                className="focus:ring-2 focus:ring-blue-500 focus:outline-none appearance-none w-full text-sm leading-6 text-slate-900 placeholder-slate-400 rounded-md py-2 pl-10 ring-1 ring-slate-200 shadow-sm"
+                                className="focus:ring-2 focus:ring-blue-500 focus:outline-none dark:focus:ring-white appearance-none w-full text-sm leading-6 text-slate-900 placeholder-slate-400 rounded-md py-2 pl-10 ring-1 ring-slate-200 shadow-sm"
                                 type="text"
                                 aria-label="Filter projects"
                                 placeholder="Filter projects..."
