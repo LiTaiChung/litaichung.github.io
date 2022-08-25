427bd8ef22c18ab2ab2ca4676a2774eda666b11e
Ice <ice@IcedeMacBook-Pro.local>
2022-08-26 00:19:02 +0800

    update footer

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export default function Footer() {
     return (
-        <footer className="p-4 bg-white shadow md:px-6 md:py-8 dark:bg-gray-900">
+        <footer className="p-4 bg-white md:px-6 md:py-8 dark:bg-gray-900">
             <div className="container mx-auto">
                 <span className="block text-sm text-gray-500 sm:text-center dark:text-white">© 2022 <Link to="/" className="hover:underline">Ice</Link>. All Rights Reserved.</span>
             </div>
