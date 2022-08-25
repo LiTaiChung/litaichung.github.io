b2f7ee5beca0cc5546b66c8f9de2feff4742aa53
Ice <ice@IcedeMacBook-Pro.local>
2022-08-26 01:05:53 +0800

    update home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,45 +1,46 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Home() {
     return (
-        <div>
-            <div
-                className="container mx-auto py-4 bg-gray relative z-10 overflow-hidden lg:flex items-center"
-            >
-                <div className="">
-                    <div className="row">
-                        <div className="w-full lg:w-1/2">
-                            <div className="header_hero_content pt-150 lg:pt-0">
-                                <h2 className="hero_title text-2xl sm:text-4xl md:text-5xl lg:text-4xl xl:text-5xl font-extrabold">
-                                    Welcome,
-                                    <div>& Hello</div>
-                                </h2>
-                                <p className="mt-8 lg:mr-8">
-                                    歡迎來到Ice的網頁
-                                    Lorem ipsum dolor sit amet, consetetur
-                                    sadipscing elitr, sed diam nonumy eirmod
-                                    tempor invidunt ut labore et dolore magna.
-                                </p>
-                                <div className="hero_btn mt-10">
-                                    <a className="main-btn" href="#0">
-                                        Get Started
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="header_shape hidden lg:block"></div>
+        <div className="h-screen bg-white dark:bg-gray-900 dark:text-white">
+            <Banner />
+        </div>
+    );
+}
 
-                <div className="header_image flex items-center">
-                    <div className="image 2xl:pl-25">
-                        <img
-                            src="https://preview.tailwindtemplates.co/plain/assets/images/header-image.svg"
-                            alt="Header Image"
-                        />
+const Banner = () => {
+    return (
+        <div className="container mx-auto py-4 bg-gray relative z-10 overflow-hidden lg:flex items-center">
+            <div className="flex-1">
+                <div className="text-center lg:text-left px-5 lg:px-0 pt-150 lg:pt-0">
+                    <h2 className="hero_title text-2xl sm:text-4xl md:text-5xl lg:text-4xl xl:text-5xl font-extrabold">
+                        Welcome!
+                    </h2>
+                    <div className="mt-8 lg:mr-8">
+                        歡迎來到Ice的網頁.
+                        此網頁以React製作
+                        <div>Welcome To Ice Website.
+                        This Website create by React</div>
                     </div>
+
+                    <div className="mt-10 mb-8 lg:mb-0">
+                        <Link to="/contact" className="bg-gray-800 text-white dark:bg-white dark:text-gray-800 px-4 py-2">
+                            More
+                        </Link>
+                    </div>
+
+                </div>
+            </div>
+
+            <div className="flex items-center">
+                <div className="image 2xl:pl-25">
+                    <img
+                        src="https://preview.tailwindtemplates.co/plain/assets/images/header-image.svg"
+                        alt="Header Image"
+                    />
                 </div>
             </div>
         </div>
-    );
+    )
 }
